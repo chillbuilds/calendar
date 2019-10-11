4110a185db39b0b71c3a235576da238da640b174
heliumface770 <heliumface770@gmail.com>
2019-10-11 12:20:24 -0600

    lock buttons update readonly status

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 $("#current").text("high");
 
+
+
+function onLoad(){
+
+};
+
+
+
 $(".lock").on("click", function(){
-    $(this).attr("src", 'images/lock-closed.png')
-    var x = document.getElementById("nine").value;
-
-alert(nine);
-    localStorage.setItem("9 am", JSON.stringify(nine));
-
-
-
+    var n = $(this).attr("lock");
+    var x = $(this).attr("number");
+    if(n === "open"){
+    $(this).attr("src", 'images/lock-closed.png');
+    document.getElementById('text' + x).readOnly = true; //setting readonly status
+    var y = document.getElementById('text' + x).value;
+    console.log("y = " + y);
+    console.log("n = " + n);
+    alert(JSON.stringify(y.value));
+    localStorage.setItem(x, JSON.stringify(y));
+    $(this).attr("lock", "closed");}
+    else{
+        $(this).attr("src", 'images/lock-open.png');
+        $(this).attr("lock", "open");
+        document.getElementById('text' + x).readOnly = false;
+    }
 });
