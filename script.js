a4fe5a700b1a84e054164fb72b0e5a7868449b6d
heliumface770 <heliumface770@gmail.com>
2019-10-11 13:05:39 -0600

    fields populating on page load

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 $("#current").text("high");
+var timeArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+onLoad ();
 
+ function onLoad() { for(var i = 0; i < timeArray.length; i++){
+    var z = JSON.parse(localStorage.getItem(timeArray[i]));
+    console.log(z);
+    $("#text" + timeArray[i]).text(z);
+ };}
 
+ 
 
-function onLoad(){
-
-};
-
-
-
-$(".lock").on("click", function(){
-    var n = $(this).attr("lock");
-    var x = $(this).attr("number");
-    if(n === "open"){
-    $(this).attr("src", 'images/lock-closed.png');
-    document.getElementById('text' + x).readOnly = true; //setting readonly status
-    var y = document.getElementById('text' + x).value;
-    console.log("y = " + y);
-    console.log("n = " + n);
-    alert(JSON.stringify(y.value));
+$(".lock").on("click", function() {
+  var n = $(this).attr("lock");
+  var x = $(this).attr("number");
+  if (n === "open") {
+    $(this).attr("src", "images/lock-closed.png");
+    var y = document.getElementById("text" + x).value;
     localStorage.setItem(x, JSON.stringify(y));
-    $(this).attr("lock", "closed");}
-    else{
-        $(this).attr("src", 'images/lock-open.png');
-        $(this).attr("lock", "open");
-        document.getElementById('text' + x).readOnly = false;
-    }
+    document.getElementById("text" + x).readOnly = true;
+    $(this).attr("lock", "closed");
+  } else {
+    $(this).attr("src", "images/lock-open.png");
+    $(this).attr("lock", "open");
+    document.getElementById("text" + x).readOnly = false;
+  }
 });
