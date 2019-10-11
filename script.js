119a65c0d209b88d8f58a8e461cd2cf7e70be666
heliumface770 <heliumface770@gmail.com>
2019-10-11 10:53:19 -0600

    html layout

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#current").text("high");
+
+$(".lock").on("click", function(){
+    $(this).attr("src", 'images/lock-closed.png')
+    var x = document.getElementById("nine").value;
+
+alert(nine);
+    localStorage.setItem("9 am", JSON.stringify(nine));
+
+
+
+});
